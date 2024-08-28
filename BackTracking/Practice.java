5d73b79bb87060fbfa079b4f36e8fb847c9429e4
lokesh <lokeshkumaravel29@gmail.com>
2024-08-28 12:10:10 +0530

    Balloon

--- BackTracking/Practice.java
+++ BackTracking/Practice.java
@@ -1,18 +1,45 @@
 import java.util.*;
 public class Practice {
     public static void main(String[] args) {
-        String str = "baloonnn";
+        String str = "loonbalxballpoon";
         char[] a = str.toCharArray();
-        Map<Character,Integer> map = new HashMap<>();
+        if(a.length<7)
+        {
+            System.out.println(0);
+        }
+       Map<Character,Integer> map = new HashMap<>();
         int min = Integer.MAX_VALUE;
         for(int i = 0;i<a.length;i++)
         {
-            map.put(a[i],map.getOrDefault(a[i], 0)+1);
-            if(map.get(a[i])<min)
+            if(a[i]=='b'||a[i]=='a'||a[i]=='l'||a[i]=='o'||a[i]=='n')
             {
-                min = map.get(a[i]);
+                map.put(a[i],map.getOrDefault(a[i], 0)+1);
             }
         }
-        System.out.println(min);
+        String t = "balon";
+        char[] te = t.toCharArray();
+        for(int i = 0;i<te.length;i++)
+        {
+            if(map.containsKey(te[i]))
+            {
+            int m = map.get(te[i]);
+            if(te[i]=='l'||te[i]=='o')
+            {
+                m/=2;
+            }
+            if(m<min){
+                min = m;
+            };
+            }
+        }
+        if(map.size()==5)
+        {
+            System.out.println(min);
+        }
+        else
+        {
+            System.out.println(0);
+        }
+        System.out.println(map.size());
     }   
 }
