810e5dd24f061defa11c1ce870ad444c9b82ecf6
lokesh <lokeshkumaravel29@gmail.com>
2024-09-01 15:32:45 +0530

    commit

---
+++ DSA/MaxWaterLevel.java
@@ -0,0 +1,34 @@
+package DSA;
+import java.util.*;
+public class MaxWaterLevel {
+
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        String[] arr = str.split(",");
+        int[] a = new int[arr.length];
+        for(int i = 0;i<arr.length;i++)
+        {
+            a[i] = Integer.parseInt(arr[i]);
+        }
+        int left = 0;
+        int right = a.length-1;
+        int maxarea = 0;
+        while(left<right)
+        {
+            int curarea = Math.min(a[left],a[right])*(right-left);
+            maxarea = Math.max(maxarea,curarea);
+            if(a[left]<a[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        s.close();
+        System.out.println(maxarea);
+    }
+}
---
+++ DSA/TargetCombiations.java
@@ -0,0 +1,45 @@
+package DSA;
+import java.util.*;
+
+public class TargetCombiations {
+    public static void main(String[] args) {
+        int n = 7;
+        int[] nums = {10,1,2,7,6,1,5};
+        int target = 8;
+        ArrayList<ArrayList<Integer>> res = check(n,nums,target);
+        System.out.println(res);
+
+    }
+    public static ArrayList<ArrayList<Integer>> check(int n,int[] a,int target)
+    {
+        Arrays.sort(a);
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        backtrack(result,new ArrayList<Integer>(),a,0,target);
+        return result;
+    }
+    public static void backtrack(ArrayList<ArrayList<Integer>> result,List<Integer> curlist,int[] a,int start,int target)
+    {
+        if(target==0)
+        {
+            if(!result.contains(curlist))
+            {
+                result.add(new ArrayList<>(curlist));
+                return;
+            }
+        }
+        for(int i = start;i<a.length;i++)
+        {
+            if(i>start&&a[i]==a[i-1])
+            {
+                continue;
+            }
+            if(a[i]>target)
+            {
+                break;
+            }
+            curlist.add(a[i]);
+            backtrack(result, curlist, a, i+1, target-a[i]);
+            curlist.remove(curlist.size()-1);
+        }
+    }
+}
