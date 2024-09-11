0d8cc3f15bddad765ce46b15ab1801bc67c25ec1
lokesh <lokeshkumaravel29@gmail.com>
2024-09-11 09:23:29 +0530

    commit

--- LeetCode/MinBitFlips.java
+++ LeetCode/MinBitFlips.java
@@ -13,3 +13,30 @@ public class MinBitFlips {
         System.out.println("Minimum number of flips : "+count);
     }
 }
+// class Solution {
+//     public int minBitFlips(int start, int goal) {
+//         String str = Integer.toBinaryString(start);
+//         String end = Integer.toBinaryString(goal);
+//         String a = "";
+//         String b = "";
+//         while(str.length()<end.length()&&a.length()+str.length()<end.length())
+//         {
+//             a+="0";
+//         }
+//         a+=str;
+//         while(str.length()>end.length()&&b.length()+end.length()<str.length())
+//         {
+//             b+="0";
+//         }
+//         b+=end;
+//         int c = 0;
+//         for(int i =0 ;i<a.length();i++)
+//         {
+//             if(a.charAt(i)!=b.charAt(i))
+//             {
+//                 c++;
+//             }
+//         }
+//         return c;
+//     }
+// }
