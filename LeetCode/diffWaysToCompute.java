78373592d0cb98b88d06f36e5324bef79fa8f87d
lokesh <lokeshkumaravel29@gmail.com>
2024-09-19 18:08:38 +0530

    /

---
+++ LeetCode/diffWaysToCompute.java
@@ -0,0 +1,44 @@
+package LeetCode;
+import java.util.*;
+
+public class diffWaysToCompute {
+    public static void main(String[] args) {
+        String expression = "2*3-4";
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < expression.length(); ++i) {
+            char oper = expression.charAt(i);
+            if (oper == '+' || oper == '-' || oper == '*') {
+                List<Integer> s1 = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> s2 = diffWaysToCompute(expression.substring(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') result.add(a + b);
+                        else if (oper == '-') result.add(a - b);
+                        else if (oper == '*') result.add(a * b);
+                    }
+                }
+            }
+        }
+        if (result.isEmpty()) result.add(Integer.parseInt(expression));
+        System.out.println(result);
+    }
+    public static List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < expression.length(); ++i) {
+            char oper = expression.charAt(i);
+            if (oper == '+' || oper == '-' || oper == '*') {
+                List<Integer> s1 = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> s2 = diffWaysToCompute(expression.substring(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') res.add(a + b);
+                        else if (oper == '-') res.add(a - b);
+                        else if (oper == '*') res.add(a * b);
+                    }
+                }
+            }
+        }
+        if (res.isEmpty()) res.add(Integer.parseInt(expression));
+        return res;
+    }
+}
