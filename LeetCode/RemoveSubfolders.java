d06b5a0f428695926da4b42acff7f2fef03d5326
Lokesh K <lokeshkumaravel29@gmail.com>
2024-10-25 11:59:51 +0530

    RemoveSubfolders

---
+++ LeetCode/RemoveSubfolders.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveSubfolders {
+    public static void main(String[] args) {
+        String[] folder = {"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
+        List<String> result = removeSubfolders(folder);
+        System.out.println(result);
+    }
+
+    public static List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> result = new ArrayList<>();
+        for (String folders : folder) {
+            if (result.isEmpty() || !folders.startsWith(result.get(result.size() - 1) + "/")) {
+                result.add(folders);
+            }
+        }
+        return result;
+    }
+}
