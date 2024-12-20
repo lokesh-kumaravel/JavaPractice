baaf0c6c31304fcabccbb87af08226b62554c197
Lokesh K <lokeshkumaravel29@gmail.com>
2024-12-20 21:45:40 +0530

    ReverseOddLevels

---
+++ DSA/ReverseOddLevels.java
@@ -0,0 +1,60 @@
+package DSA;
+
+public class ReverseOddLevels {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Original tree:");
+        printTree(root);
+
+        Solution solution = new Solution();
+        solution.reverseOddLevels(root);
+
+        System.out.println("\nModified tree:");
+        printTree(root);
+    }
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.val);
+        printTree(root.left);
+        printTree(root.right);
+    }
+}
+
+class Solution {
+    public void reverseOddLevels(TreeNode root) {
+        traverse(root.left, root.right, 1);
+    }
+
+    public void traverse(TreeNode node1, TreeNode node2, int lvl) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        if (lvl % 2 == 1) {
+            int temp = node1.val;
+            node1.val = node2.val;
+            node2.val = temp;
+        }
+
+        traverse(node1.left, node2.right, lvl + 1);
+        traverse(node1.right, node2.left, lvl + 1);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
