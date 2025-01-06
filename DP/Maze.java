e87008905326073cb98c98739b3815ec8b76ca8e
Lokesh K <lokeshkumaravel29@gmail.com>
2025-01-06 18:56:48 +0530

    commit

--- DP/Maze.java
+++ DP/Maze.java
@@ -3,33 +3,26 @@ package DP;
 import java.util.Scanner;
 
 public class Maze {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         long n = s.nextLong();
         long m = s.nextLong();
         s.close();
-        long[][] a = new long[(int)n+1][(int)m+1];
-        for(long i = 1;i<=n;i++)
-        {
-            for(long j = 1;j<=m;j++)
-            {
-                if(i==1||j==1)
-                {
-                    a[(int)i][(int)j] = 1;
+        long[][] a = new long[(int) n + 1][(int) m + 1];
+        for (long i = 1; i <= n; i++) {
+            for (long j = 1; j <= m; j++) {
+                if (i == 1 || j == 1) {
+                    a[(int) i][(int) j] = 1;
                 }
             }
         }
-        for(long i = 1;i<=n;i++)
-        {
-            for(long j = 1;j<=m;j++)
-            {
-                if(a[(int)i][(int)j]==0)
-                {
-                    a[(int)i][(int)j] = a[(int)i][(int)j-1]+a[(int)i-1][(int)j];
+        for (long i = 1; i <= n; i++) {
+            for (long j = 1; j <= m; j++) {
+                if (a[(int) i][(int) j] == 0) {
+                    a[(int) i][(int) j] = a[(int) i][(int) j - 1] + a[(int) i - 1][(int) j];
                 }
             }
         }
-        System.out.println(a[(int)n][(int)m]);
+        System.out.println(a[(int) n][(int) m]);
     }
 }
