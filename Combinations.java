dfc3b24b1b5b7ed5bb4e5fbf6670331dcde11dcb
Lokesh K <lokeshkumaravel29@gmail.com>
2025-01-22 22:00:42 +0530

    TargetCombination

--- Combinations.java
+++ Combinations.java
@@ -22,6 +22,7 @@ class Combination {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the input value");
         int n = s.nextInt();
         int arr[] = new int[n];
         for (int i = 0; i < n; i++) {
